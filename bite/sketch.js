52068e0641b9327f1b84aa1345247ee596b53bea
Karina <karina.popp@nyu.edu>
2024-05-30 13:57:52 -0400

    lips n gums

--- bite/sketch.js
+++ bite/sketch.js
@@ -51,24 +51,10 @@ let sketch = function (p) {
 
     var inMouth = false;
 
-    var topLipX1
-    var topLipY1
-    var topLipX2
-    var topLipY2
+    var topLip;
+    var bottomLip;
 
-    var topLipConX1
-    var topLipConY1
-    var topLipConX2
-    var topLipConY2
-
-    var topLipConX1Base
-    var topLipConY1Base
-
-    var topLipConX1Tar
-    var topLipConY1Tar
-
-    var topLipConX1Mod = 0
-    var topLipConY1Mod = 0
+    var topGums
 
     p.preload = function () {
         winWidth = window.innerWidth;
@@ -78,6 +64,8 @@ let sketch = function (p) {
         botTeethX = topTeethX + 30;
         botTeethY = winHeight * 0.55;
         teefsImg = p.loadImage('teefs.png');
+
+        topGums = p.loadImage('topgums.png');
     }
 
     p.setup = function () {
@@ -96,29 +84,18 @@ let sketch = function (p) {
         mouthW = 420
         mouthH = 340
 
-        topLipX1 = topTeethX - topTeethW[0]/2
-        topLipY1 = topTeethY - topTeethH/2
-        topLipX2 = topTeethX - topTeethW[0]/2 + 420
-        topLipY2 = topTeethY - topTeethH/2
-
-        topLipConX1 = topLipX1 + 50
-        topLipConY1 = topLipY1 + 180
-        topLipConX2 = topLipX2 - 50
-        topLipConY2 = topLipY2 + 180
-
-        topLipConX1Base = topLipConX1
-        topLipConY1Base = topLipConY1
-
-        topLipConX1Tar = topLipConX1Base
-        topLipConY1Tar = topLipConY1Base
+        topLip = new p.Lip(topTeethX - topTeethW[0]/1.5, topTeethY, topTeethX - topTeethW[0]/2 + 430, topTeethY, 0, -80);
+        bottomLip = new p.Lip(botTeethX - botTeethW[0]/0.7, botTeethY, botTeethX - botTeethW[0]/2 + 400, botTeethY, 500, 80);
     }
 
     p.draw = function () {
         p.background(234, 203, 210);
+        p.noStroke();
         p.fill(37, 40, 61);
         p.rect(mouthX, mouthY, mouthW, mouthH)
         p.fill(184, 72, 154)
-        p.rect(topTeethX - topTeethW[0]/2, topTeethY - topTeethH/2 - 20, 420, 50);
+        p.image(topGums, topTeethX + 180, topTeethY - 40)
+        //p.rect(topTeethX - topTeethW[0]/2, topTeethY - topTeethH/2 - 20, 420, 50);
         p.rect(botTeethX - botTeethW[0]/2, botTeethY + botTeethH/2 - 20, 360, 50);
         for(var i = 0; i < botTeeth.length; i++) {
             botTeeth[i].draw();
@@ -126,30 +103,12 @@ let sketch = function (p) {
         for(var i = 0; i < topTeeth.length; i++) {
             topTeeth[i].draw();
         }
-        p.strokeWeight(100);
-        //p.stroke(234, 203, 210);
-        p.noFill();
-        p.stroke(255);
-        //p.line(topLipX1, topLipY1, topLipX2, topLipY2)
-        p.curve(topLipConX1, topLipConY1, topLipX1, topLipY1, topLipX2, topLipY2, topLipConX2, topLipConY2);
-        p.noStroke();
 
-        p.fill(255, 0, 0);
-        p.ellipse(topLipConX1, topLipConY1, 20)
-        p.ellipse(topLipConX2, topLipConY2, 20)
-
-        topLipConX1 = p.lerp(topLipConX1, topLipConX1Tar, 0.05)
-        topLipConY1 = p.lerp(topLipConY1, topLipConY1Tar, 0.05)
-
-        if(p.mouseX > topLipX1 && p.mouseX < topLipX1 + (topLipX2 - topLipX1)/2  && p.mouseY > topLipY1 - 50 && p.mouseY < topLipY1 + 150) {
-            topLipConX1Mod = 50 * (-1 * (p.mouseX - p.pmouseX))
-            topLipConY1Mod = 50 * (p.mouseY - p.pmouseY)
-            topLipConX1Tar = topLipConX1Base + topLipConX1Mod
-            topLipConY1Tar = topLipConY1Base + topLipConY1Mod
-        } else {
-            topLipConX1Tar = topLipConX1Base
-            topLipConY1Tar = topLipConY1Base
-        }
+        topLip.touch();
+        topLip.draw();
+       
+        bottomLip.touch();
+        bottomLip.draw();
     }
 
     p.mouseDragged = function() {
@@ -193,23 +152,75 @@ let sketch = function (p) {
         }
     }
 
+    p.Lip = class {
+        constructor(_x1, _y1, _x2, _y2, _anchorY1, _conY){
+            this.firstVec = p.createVector(_x1, _y1);
+            this.secondVec = p.createVector(_x2, _y2);
+            this.firstCon = p.createVector(_x1 + 80, _y1 + _conY)
+            this.secondCon = p.createVector(_x2 - 50, _y2 + _conY)
+
+            this.firstConBase = p.createVector(_x1 + 80, _y1 + _conY)
+            this.firstConTar = p.createVector(_x1 + 80, _y1 + _conY)
+            this.firstConMod = p.createVector(0, 0)
+
+            this.secondConBase = p.createVector(_x2 - 50, _y2 + _conY)
+            this.secondConTar = p.createVector(_x2 - 50, _y2 + _conY)
+            this.secondConMod = p.createVector(0, 0)
+
+            this.anchorY = _anchorY1;
+        }
+
+        draw() {
+            p.fill(234, 203, 210);
+            p.beginShape();
+            p.vertex(mouthX, this.anchorY);
+            p.vertex(this.firstVec.x, this.firstVec.y);
+            p.bezierVertex(this.firstCon.x, this.firstCon.y, this.secondCon.x, this.secondCon.y, this.secondVec.x, this.secondVec.y)
+            p.vertex(mouthX + mouthW, this.anchorY)
+            p.endShape();
+
+            p.fill(255, 0, 0);
+            p.ellipse(this.firstCon.x, this.firstCon.y, 20)
+            p.ellipse(this.secondCon.x, this.secondCon.y, 20)
+        }
+
+        touch() {
+
+            this.firstCon.x = p.lerp(this.firstCon.x, this.firstConTar.x, 0.05)
+            this.firstCon.y = p.lerp(this.firstCon.y, this.firstConTar.y, 0.05)
+
+            this.secondCon.x = p.lerp(this.secondCon.x, this.secondConTar.x, 0.05)
+            this.secondCon.y = p.lerp(this.secondCon.y, this.secondConTar.y, 0.05)
+    
+            if(p.mouseX > this.firstVec.x && p.mouseX < this.firstVec.x + (this.secondVec.x - this.firstVec.x)/2  && p.mouseY > this.firstVec.y - 50 && p.mouseY < this.firstVec.y + 150) {
+                this.firstConMod.y = this.firstConBase.y - p.mouseY
+                //this.firstConTar.x = this.firstConBase.x + this.firstConMod.x
+                this.firstConTar.y = this.firstConBase.y + this.firstConMod.y
+            } else {
+                this.firstConTar.set(this.firstConBase)
+            }
+
+            if(p.mouseX > this.secondVec.x - (this.secondVec.x - this.firstVec.x)/2 && p.mouseX < this.secondVec.x && p.mouseY > this.secondVec.y - 50 && p.mouseY < this.secondVec.y + 150){
+                this.secondConMod.y = this.secondConBase.y - p.mouseY
+                this.secondConTar.y = this.secondConBase.y + this.secondConMod.y
+            } else {
+                this.secondConTar.set(this.secondConBase)
+            }
+        }
+    }
+
     p.Tooth = class {
         constructor(_x, _y, _w, _h, _i, _t) {
-            this.x = _x;
-            this.y = _y;
-            this.w = _w;
-            this.h = _h;
+            this.pos = p.createVector(_x,_y);
+            this.size = p.createVector(_w, _h)
             this.red = 255;
             this.green = 255;
             this.blue = 255;
             this.held = false;
             this.state = Gums
             
-            this.yVel = 0;
-            this.yMax = 10;
-
-            this.xVel = 0;
-            this.xMax = 15;
+            this.vel = p.createVector(0, 0);
+            this.maxVel = p.createVector(15, 10);
 
             this.onGround = false;
             this.xDecel = 0.08;
@@ -217,8 +228,7 @@ let sketch = function (p) {
             this.resist = 20;
             this.break = 50;
 
-            this.startX = _x;
-            this.startY = _y;
+            this.startPos = p.createVector(_x, _y);
 
             this.gumDist = 5;
             this.threadDist = 80;
@@ -234,57 +244,18 @@ let sketch = function (p) {
             this.isTop = _t;
         }
 
-        draw() {
-            if (this.state != Pulled && this.state != Released) {
-                p.strokeWeight(30)
-                p.stroke(184, 72, 154);
-                if(this.isTop) {
-                    p.line(this.startX, this.startY - (this.h/2), this.x, this.y - (this.h/2) + 20);
-                } else  {
-                    p.line(this.startX, this.startY + (this.h/2), this.x, this.y + (this.h/2));
-                }
-                
-            }
-
+        update() {
             switch(this.state) {
                 case Gums:
-                    if(inMouth) {
-                        this.currDistX = p.abs(this.startX - p.mouseX);
-                        this.currDistY = p.abs(this.startY - p.mouseY);
-                        if (this.currDistX >= 1) {
-                            this.currDistX = 1;
-                        }
-                        if (this.currDistY >= 1) {
-                            this.currDistY = 1;
-                        }
-                        var diffX = this.startX - p.mouseX;
-                        var diffY = this.startY - p.mouseY;
-                        this.angle = p.atan2(-1 * diffY, diffX) - p.radians(90);
-                        //this.yVel += p.constrain(passiveMouseSpeed, -1, 1);
-                        //this.xVel += p.constrain(passiveMouseSpeed, -1, 1);
-                        this.threadPhysics();
-                    }
+                    this.startDistCheck(1);
                     break;
                 case Held:
                     if (p.abs(mouseSpeed) > this.resist) {
                         this.state = ThreadedLoose;
-                        this.yVel = 1;
-                        this.xVel = p.constrain(mouseSpeed, -this.xMax, this.xMax);
+                        this.vel.set(p.constrain(mouseSpeed, -this.maxVel.x, this.maxVel.x), 1);
                         hasTooth = false;
-                        //this.state = Pulled;
                     } else {
-                        this.currDistX = p.abs(this.startX - p.mouseX);
-                        this.currDistY = p.abs(this.startY - p.mouseY);
-                        if (this.currDistX >= this.gumDist) {
-                            this.currDistX = this.gumDist;
-                        }
-                        if (this.currDistY >= this.gumDist) {
-                            this.currDistY = this.gumDist;
-                        }
-                        var diffX = this.startX - p.mouseX;
-                        var diffY = this.startY - p.mouseY;
-                        this.angle = p.atan2(-1 * diffY, diffX) - p.radians(90);
-                        this.threadPhysics();
+                        this.startDistCheck(this.gumDist);
                     }
                     break;
                 case ThreadedLoose:
@@ -295,51 +266,63 @@ let sketch = function (p) {
                     this.aVel += this.aAcc;
                     this.aVel *= this.damp;
                     this.angle += this.aVel;
-                    this.threadPhysics();
                     break;
                 case ThreadedHeld:
                     if (p.abs(mouseSpeed) > this.break) {
                         this.state = Released;
                         hasTooth = false;
-                        this.yVel = 1;
-                        this.xVel = p.constrain(mouseSpeed, -this.xMax, this.xMax);
+                        this.vel.set(p.constrain(mouseSpeed, -this.maxVel.x, this.maxVel.x), 1);
                     } else {
-                        this.currDistX = p.abs(this.startX - p.mouseX);
-                        this.currDistY = p.abs(this.startY - p.mouseY);
-                        if (this.currDistX >= this.threadDist) {
-                            this.currDistX = this.threadDist;
-                        }
-                        if (this.currDistY >= this.threadDist) {
-                            this.currDistY = this.threadDist;
-                        }
-                        var diffX = this.startX - p.mouseX;
-                        var diffY = this.startY - p.mouseY;
-                        this.angle = p.atan2(-1 * diffY, diffX) - p.radians(90);
-                        this.threadPhysics();
+                        this.startDistCheck(this.threadDist);
                     }
                     break;
                 case Pulled:
-                    this.x = p.mouseX;
-                    this.y = p.mouseY;
+                    this.pos.set(p.mouseX, p.mouseY);
                     break;
                 case Released:
-                    this.physics();
+                    
                     break;
             }
+            this.physics();
+        }
 
+        draw() {
+            if (this.state != Pulled && this.state != Released) {
+                p.strokeWeight(30)
+                p.stroke(184, 72, 154);
+                if(this.isTop) {
+                    p.line(this.startPos.x, this.startPos.y - (this.size.y/2), this.pos.x, this.pos.y - (this.size.y/2) + 20);
+                } else  {
+                    p.line(this.startPos.x, this.startPos.y + (this.size.y/2), this.pos.x, this.pos.y + (this.size.y/2));
+                }
+                
+            }
+           this.update();
             p.noStroke();
             p.fill(this.red, this.green, this.blue);
             if(this.isTop) {
-                p.image(topSprites[this.index], this.x, this.y)
+                p.image(topSprites[this.index], this.pos.x, this.pos.y)
             } else {
-                p.image(botSprites[this.index], this.x, this.y)
+                p.image(botSprites[this.index], this.pos.x, this.pos.y)
             }
-            if(debug) p.ellipse(this.x, this.y, this.w, this.h);
+            if(debug) p.ellipse(this.pos.x, this.pos.y, this.size.x, this.size.y);
+        }
+
+        startDistCheck(distReset) {
+            this.currDistX = p.abs(this.startPos.x - p.mouseX);
+            this.currDistY = p.abs(this.startPos.y - p.mouseY);
+            if (this.currDistX >= distReset) {
+                this.currDistX = distReset;
+            }
+            if (this.currDistY >= distReset) {
+                this.currDistY = distReset;
+            }
+            this.angle = p.atan2(-1 * (this.startPos.y - p.mouseY), this.startPos.x - p.mouseX) - p.radians(90);
         }
 
         checkCol() {
-            if(p.mouseX < this.x + this.w/2 && p.mouseX > this.x - this.w/2 &&
-                p.mouseY < this.y + this.h/2 && p.mouseY > this.y - this.h/2
+            if(p.mouseX < this.pos.x + this.size.x/2 && p.mouseX > this.pos.x - this.size.x/2 &&
+                p.mouseY < this.pos.y + this.size.y/2 && p.mouseY > this.pos.y - this.size.y/2
             ) {
                 if (this.state == Gums) {
                     hasTooth = true;
@@ -366,83 +349,49 @@ let sketch = function (p) {
             } else if (this.state == Pulled) {
                 hasTooth = false;
                 this.state = Released
-                this.yVel = 1;
-                this.xVel = p.constrain(mouseSpeed, -this.xMax, this.xMax);
+                this.vel.set(p.constrain(mouseSpeed, -this.maxVel.x, this.maxVel.x), 1);
             } else if (this.state == ThreadedHeld) {
                 this.state = ThreadedLoose;
-                this.yVel = 1;
-                this.xVel = p.constrain(mouseSpeed, -this.xMax, this.xMax);
+                this.vel.set(p.constrain(mouseSpeed, -this.maxVel.x, this.maxVel.x), 1);
                 hasTooth = false;
             }
         }
 
         threadPhysics() {
-    
-            this.x = this.currDistX * p.sin(this.angle);
-            this.y = this.currDistY * p.cos(this.angle);
-            this.x += this.startX;
-            this.y += this.startY;
-            
-
-            // if(this.y < winHeight - this.r/2) {
-            //     if(this.yVel < this.yMax) {
-            //         this.yVel+=gravity;
-            //     }
-            //     // if(this.y >= winHeight - this.r/2) {
-            //     //     this.y = winHeight - this.r/2;
-            //     //     this.onGround = true;
-            //     // }
-            //     if (this.y - this.startY < this.threadDist) {
-            //         this.y += this.yVel;
-            //     }
-            // }
-            // if(this.x < winWidth - this.r/2 && this.x > this.r/2) {
-            //     if(this.onGround) {
-            //         this.xVel = this.xVel - (Math.sign(this.xVel) * this.xDecel);
-            //     }
-            //     if (p.abs(this.x - this.startX) < this.threadDist) {
-            //         this.x += this.xVel;
-            //     }
-            //     if (this.x < this.startX - this.threadDist) {
-            //         console.log("limit left")
-            //         this.x = (this.startX - this.threadDist) + 2;
-            //         this.xVel = -this.xVel;
-            //         this.xVel *= 0.3
-            //     }
-            //     if(this.x > this.startX + this.threadDist) {
-            //         this.x = (this.startX + this.threadDist) - 2;
-            //         this.xVel = -this.xVel; 
-            //         this.xVel *= 0.3
-            //     }
-            // }
         }
 
         physics () {
-            if(this.y < winHeight - this.h/2) {
-                if(this.yVel < this.yMax) {
-                    this.yVel+=gravity;
+            if(this.state == Released){
+                if(this.pos.x < winWidth - this.size.x/2 && this.pos.x > this.size.x/2) {
+                    if(this.onGround) {
+                        this.vel.x = this.vel.x - (Math.sign(this.vel.x) * this.xDecel);
+                    }
+
+                } else {
+                    this.vel.x = 0;
+                    if(this.pos.x < this.size.x/2) {
+                        this.pos.x = this.size.x/2 + 2;
+                    }
+                    if(this.pos.x > winWidth - this.size.x/2) {
+                        this.pos.x = winWidth - this.size.x/2 - 2;
+                    }
                 }
-                this.y += this.yVel;
-                if(this.y >= winHeight - this.h/2) {
-                    this.y = winHeight - this.h/2;
+                if(this.pos.y < winHeight - this.size.y/2) {
+                    if(this.vel.y < this.maxVel.y) {
+                        this.vel.y+=gravity;
+                    }
+                } else {
+                    this.vel.y = 0;
                     this.onGround = true;
+                    if(this.pos.y >= winHeight - this.size.y/2) {
+                        this.pos.y = winHeight - this.size.y/2;
+                    }
                 }
-            }
-            if(this.x < winWidth - this.w/2 && this.x > this.w/2) {
-                if(this.onGround) {
-                    this.xVel = this.xVel - (Math.sign(this.xVel) * this.xDecel);
-                }
-                this.x += this.xVel;
-                if(this.x < this.w/2) {
-                    this.x = this.w/2 + 2;
-                    this.xVel = -this.xVel;
-                    this.xVel *= 0.7
-                }
-                if(this.x > winWidth - this.w/2) {
-                    this.x = winWidth - this.w/2 - 2;
-                    this.xVel = -this.xVel; 
-                    this.xVel *= 0.7
-                }
+                this.pos.add(this.vel)
+            } else if(this.state != Pulled) {
+                this.pos.x = this.currDistX * p.sin(this.angle);
+                this.pos.y = this.currDistY * p.cos(this.angle);
+                this.pos.add(this.startPos);
             }
         }
 
